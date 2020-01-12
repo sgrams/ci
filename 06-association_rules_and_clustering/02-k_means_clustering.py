e89556fa2b0960b60ba3338e2176b20ac6128e06
Stanisław Grams <sjg@fmdx.pl>
2020-01-12 14:50:51 +0100

    src: update 02-k_means_clustering.py to indicate number of clusters...

--- 06-association_rules_and_clustering/02-k_means_clustering.py
+++ 06-association_rules_and_clustering/02-k_means_clustering.py
@@ -17,8 +17,9 @@ kmeans = KMeans (n_clusters=3, init='k-means++', max_iter=300, n_init=10, random
 y_kmeans = kmeans.fit_predict (x);
 
 ## scatter chart
-plt.scatter (x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], s=100, c='red', label='PC1')
-plt.scatter (x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], s=100, c='green', label='PC2')
+plt.scatter (x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], s=100, c='red', label='Cluster 1')
+plt.scatter (x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], s=100, c='green', label='Cluster 2')
+plt.scatter (x[y_kmeans == 2, 0], x[y_kmeans == 2, 1], s=100, c='blue', label='Cluster 3')
 plt.scatter (kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:,1], s=100, c='magenta', label='Centroids')
 plt.legend ()
 plt.show ()
